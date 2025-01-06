d29febd026cd30b27d446df50d7a7f3badc88a48
Otavio Prada <otaviovinicius07@hotmail.com>
2025-01-06 14:28:24 -0300

    feat: estilização dos componentes

---
+++ src/components/pages/App.tsx
@@ -0,0 +1,15 @@
+import Formulario from '../formulario/index.tsx'
+import Lista from '../lista/index.tsx';
+import './styles.scss'
+
+function App() {
+
+  return (
+    <div className='AppStyle'>
+      <Formulario />
+      <Lista/>
+    </div>
+  );
+};
+
+export default App;
