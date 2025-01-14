cf556f31ae95b818c9488dfa410f852e92d04f87
Otavio Prada <otaviovinicius07@hotmail.com>
2025-01-14 16:27:36 -0300

    feat: cronometro

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,5 +1,6 @@
-import Formulario from '../formulario/index.tsx'
-import Lista from '../lista/index.tsx';
+import { Cronometro } from '../Cronometro/index.tsx';
+import Formulario from '../Formulario/index.tsx'
+import Lista from '../Lista/index.tsx';
 import style from './App.module.scss'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className={style.AppStyle}>
       <Formulario />
       <Lista/>
+      <Cronometro/>
     </div>
   );
 };
